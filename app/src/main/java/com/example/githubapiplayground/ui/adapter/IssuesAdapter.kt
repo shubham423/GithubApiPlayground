edb72da036f8b3b5e5e9e359cf413218af1448bb
shubham423 <shubh991172@gmail.com>
2022-11-02 20:13:11 +0530

    ui fixes

--- app/src/main/java/com/example/githubapiplayground/ui/adapter/IssuesAdapter.kt
+++ app/src/main/java/com/example/githubapiplayground/ui/adapter/IssuesAdapter.kt
@@ -3,21 +3,25 @@ package com.example.githubapiplayground.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.githubapiplayground.data.models.Issue
 import com.example.githubapiplayground.databinding.ItemIssueBinding
 
 class IssuesAdapter(val data: List<Issue>) : RecyclerView.Adapter<IssuesAdapter.ViewHolder>() {
 
-    class ViewHolder(private val binding: ItemIssueBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private val binding: ItemIssueBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(issue: Issue) {
-            binding.titleTv.text=issue.title
-            binding.closedDateTv.text=issue.closedAt
-            binding.openedDateTv.text=issue.createdAt
+            binding.titleTv.text = issue.title
+            binding.closedDateTv.text = issue.closedAt
+            binding.openedDateTv.text = issue.createdAt
+            Glide.with(binding.root).load(issue.user.avatarUrl).into(binding.userImg)
+            binding.usernameTv.text=issue.user.login
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding=ItemIssueBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = ItemIssueBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
